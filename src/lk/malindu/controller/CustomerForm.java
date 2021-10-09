4635f80caf40907f243f75e6fb8da979f5feb390
malindu amesh <malindu@misynergy.com>
2021-10-09 11:12:54 +0530

    adding customer model and view

---
+++ src/lk/malindu/controller/CustomerForm.java
@@ -0,0 +1,16 @@
+package lk.malindu.controller;
+
+
+import lk.malindu.model.Customer;
+import lk.malindu.model.DBConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CustomerForm {
+
+    public static boolean addCustomer(Customer customer) throws SQLException, ClassNotFoundException {
+
+
+    }
+}
---
+++ src/lk/malindu/controller/CustomerFormController.java
@@ -0,0 +1,28 @@
+package lk.malindu.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import lk.malindu.model.Customer;
+
+import javax.swing.*;
+import java.sql.SQLException;
+
+public class CustomerFormController {
+    public Button btnSaveID;
+    public Button btnCancelId;
+
+    public TextField txtID;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtContact;
+
+
+    public void btnSave(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+
+
+    }
+
+    public void btnCancel(ActionEvent actionEvent) {
+    }
+}
